ed960820d945451de89ce791befa87aa6236c389
pgatic <pgatic@hotmail.com>
2020-10-19 11:45:11 +0200

    added whoami endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,19 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+// /api/whoami endpoint...
+app.get("/api/whoami", function (req, res) {
+  var langs = req.header('Accept-Language');
+  var sw = req.header('User-Agent');
+  res.json({
+    ipaddress: req.ip,
+    languages: langs,
+    software: sw
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
